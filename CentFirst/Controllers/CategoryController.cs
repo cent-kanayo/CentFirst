27cb1ac75d2c1f765908466de169c7f58480057b
HP <HP@CENT-KANAYO>
2023-07-06 22:01:57 +0100

    Add project files.

---
+++ CentFirst/Controllers/CategoryController.cs
@@ -0,0 +1,99 @@
+﻿using CentFirst.Data;
+using CentFirst.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CentFirst.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly DbConfig _db;
+        public CategoryController(DbConfig db) {
+            _db = db;
+        }
+        public IActionResult Index()
+        
+        {
+            List<Category> objCategories = _db.Categories.ToList();
+            return View(objCategories);
+        }
+        
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj) 
+        {
+            if(obj.Name == obj.DisplayOrder.ToString()) {
+                ModelState.AddModelError("", "Name cannot be the same as Display Order");           
+            }
+          if (ModelState.IsValid){
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category added successfully!";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id < 1)
+            {
+                return NotFound();  
+            }
+            Category? category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully!";
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id < 1)
+            {
+                return NotFound();
+            }
+            Category? category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+
+            Category? category = _db.Categories.Find(id);
+
+            if (category == null)
+            {
+                return View();
+            }
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully!";
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ CentFirst/Data/DbConfig.cs
@@ -0,0 +1,22 @@
+﻿using CentFirst.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CentFirst.Data
+{
+    public class DbConfig : DbContext
+    {
+        public DbConfig(DbContextOptions<DbConfig> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category{Id = 1, Name = "Action", DisplayOrder = 1},
+                new Category{Id = 2, Name = "SCI-FI", DisplayOrder = 2},
+                new Category{Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+    }
+}
